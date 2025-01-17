a096aaddbd6ca32c4a5eca33153dcde4beb54452
ZJ <1739593209@qq.com>
2025-01-17 16:35:07 +0800

    Loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -5,13 +5,16 @@ const LoadingCom = () => {
       <div className="preloader_left"></div>
       <div className="preloader_right"></div>
       <div className="preloader">
-        <span className="inner">S</span>
-        <span className="inner">H</span>
-        <span className="inner">I</span>
-        <span className="inner">M</span>
-        <span className="inner">M</span>
-        <span className="inner">E</span>
-        <span className="inner">R</span>
+        <span className="inner">L</span>
+        <span className="inner">o</span>
+        <span className="inner">a</span>
+        <span className="inner">d</span>
+        <span className="inner">i</span>
+        <span className="inner">n</span>
+        <span className="inner">g</span>
+        <span className="inner">.</span>
+        <span className="inner">.</span>
+        <span className="inner">.</span>
       </div>
     </div>
   );
