91770517ce911a1b98c3baba0bb93a24f1f9589a
张洁 <1739593209@qq.com>
2025-01-29 17:18:28 +0800

    好耶

--- app/pages/works/page.tsx
+++ app/pages/works/page.tsx
@@ -1,46 +1,119 @@
 "use client";
-import Link from "next/link";
+
 import Image from "next/image";
-import { useEffect } from "react";
-import {
-  addNavItemStyle,
-  bindHandleScroll,
-  removeNavItemStyle,
-  removeScroll,
-} from "@utils/elementUtils";
-import withLoading from "@components/WithLoading";
-import useChangeLoading from "@components/WithLoading/useChangeLoading";
+import Link from "next/link";
+import { useEffect, useRef } from "react";
+import { bindHandleScroll, removeScroll } from "@utils/elementUtils";
+import { scrollTo } from "@utils/element";
+import SysIcon from "@components/SysIcon";
+// 图片
 import works_bg1 from "@public/images/2.png";
-import display_1 from "@public/display/tijian.png";
-import display_2 from "@public/display/echart.png";
+import display_1 from "@public/display/juhui.png";
+import display_2 from "@public/display/tijian.png";
 import display_3 from "@public/display/jhdatav.png";
-import display_4 from "@public/display/wanyiyun.png";
 
-import { SearchOutlined } from "@ant-design/icons";
-import { Button } from "antd";
+import { loadingImag } from "@utils/dataImage";
+import styles from "./works.module.css";
 
-import style from "./works.module.css";
-import { Carousel } from "antd";
+export default function Home() {
+  const aboutDom = useRef<any>(null);
 
-const Works: React.FC = (props) => {
+  // 点击下部箭头
+  const goAbout = () => {
+    const aboutTop = aboutDom.current.offsetTop;
+    scrollTo(aboutTop, {
+      getContainer: () => document.body || window,
+    });
+  };
+
   useEffect(() => {
-    addNavItemStyle();
     bindHandleScroll();
-
     return () => {
-      removeNavItemStyle();
       removeScroll();
     };
   }, []);
 
-  useChangeLoading({ ...props, name: "works" });
-
   return (
-    <div className={style.shell}>
-      <Image className={style.image} src={works_bg1} alt="works_bg1" />
-      <div className={style.content}>作品集</div>
+    <div className={styles.home}>
+      <div className={styles.bg_card}>
+        <Image
+          className={styles.bg_card_img}
+          width={2000}
+          height={1000}
+          src={works_bg1}
+          alt="blog-bg"
+          priority={true}
+        />
+      </div>
+      <div className={styles.bg_mask} id="bg_mask" />
+      <div className={styles.bg_content}>
+        <div className={styles.title}>参与&完成项目</div>
+        <div className={styles.jiantou}>
+          <SysIcon
+            className={styles.jiantou_icon}
+            type="icon-a-jiantou-xia"
+            onClick={goAbout}
+          />
+        </div>
+      </div>
+      <div className={styles.page_box} ref={aboutDom}>
+        <div className={styles.page_title}>Complete Project</div>
+        <div className={styles.page_desc}>项目集</div>
+        <div className={styles.page_list}>
+          <div className={styles.page_item}>
+            <Image
+              className={styles.page_item_bg}
+              width={2000}
+              height={1320}
+              src={display_1}
+              alt="剧荟广场"
+              placeholder="blur"
+              blurDataURL={loadingImag}
+              priority={true}
+            />
+            <Link className={styles.page_item_link} href="">
+              剧 荟 广 场 小 程 序
+            </Link>
+          </div>
+          <div className={styles.page_item}>
+            <Image
+              className={styles.page_item_bg}
+              width={2000}
+              height={1320}
+              src={display_2}
+              alt="体检预约"
+              placeholder="blur"
+              blurDataURL={loadingImag}
+              priority={true}
+            />
+            <Link className={styles.page_item_link} href="">
+              体 检 预 约 H5 端
+            </Link>
+          </div>
+
+          <div className={styles.page_item}>
+            <Image
+              className={styles.page_item_bg}
+              width={2000}
+              height={1320}
+              src={display_3}
+              alt="数据大屏"
+              placeholder="blur"
+              blurDataURL={loadingImag}
+              priority={true}
+            />
+            <Link className={styles.page_item_link} href="">
+              数 据 大 屏
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles.timeAixs_box}>
+        <div className={styles.timeAixs_title}>KNOW MORE</div>
+        <div className={styles.timeAixs_desc}>
+          「 想了解更多？请查看个人简历 」
+        </div>
+      </div>
     </div>
   );
-};
-
-export default withLoading(Works);
+}
